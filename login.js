cb1cad1338222f923cf075c26c3b97574499fc1a
Usama <usamariaz@aptechnorth.edu.pk>
2026-03-28 20:56:38 +0500

    till login and signup

---
+++ login.js
@@ -0,0 +1,65 @@
+let loginBtn = document.getElementById('loginBtn');
+let message = document.getElementById('message');
+
+
+
+
+
+
+
+
+let usersData = JSON.parse(localStorage.getItem('users'));
+console.log(usersData)
+
+
+loginBtn.addEventListener('click',()=>{
+ 
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginPassword = document.getElementById('loginPassword').value;
+   
+   
+    let foundUser = usersData.find(user => user.email == loginEmail);
+   
+    if(foundUser){
+        if(loginPassword == foundUser.password){
+            console.log('Login Successfull!');
+
+
+            // message.innerText = 'Login Successfull!'
+
+            location.href = 'home.html'
+
+
+        }
+        else
+        {
+            console.log('Password is incorrect!')
+            message.innerText = 'Password is incorrect!'
+        }
+    }
+    else
+    {
+        console.log("User not Found!")
+        message.innerText = 'User not Found!'
+
+    }
+
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   })
+   
+   
---
+++ script.js
@@ -0,0 +1,52 @@
+// button selection
+let signupBtn = document.getElementById("signupBtn");
+let alertMessage = document.getElementById('alertMessage');
+let signupForm = document.getElementById('signupForm')
+
+
+
+const users = [];
+
+// Signup Functionality
+
+signupBtn.addEventListener("click", () => {
+  // all input feilds selections
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let city = document.getElementById("city").value;
+  let country = document.getElementById("country").value;
+  let address = document.getElementById("address").value;
+
+  const userObj = {
+    firstName,
+    lastName,
+    email,
+    password,
+    city,
+    country,
+    address,
+  };
+
+
+  users.push(userObj)
+
+  localStorage.setItem('users',JSON.stringify(users));
+  
+  console.log(users);
+
+  alertMessage.classList.remove('hidden')
+
+  signupForm.reset()
+
+
+//   console.log(userObj);
+});
+
+
+
+
+
+
+
